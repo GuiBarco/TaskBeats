9e7a926e3c2f9c0f030779d4207d3ac70a0970f2
GuiBarco <hebertbarco@hotmail.com>
2024-03-26 17:04:46 -0300

    create package layers

--- app/src/test/java/com/comunidadedevspace/taskbeats/ExampleUnitTest.kt
+++ app/src/test/java/com/comunidadedevspace/taskbeats/ExampleUnitTest.kt
@@ -2,7 +2,7 @@ package com.comunidadedevspace.taskbeats
 
 import org.junit.Test
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 
 /**
  * Example local unit test, which will execute on the development machine (host).
